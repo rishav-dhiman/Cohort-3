1accd715d9436db1a73b17c7e3094930027ea3c4
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-01-21 13:27:52 +0530

    Create 05_functional_arguements_calculator.js

---
+++ Web Dev/Week 02/05_functional_arguements_calculator.js
@@ -0,0 +1,15 @@
+// creating a calculator
+function sum(a, b){
+    return a + b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function subtract(a, b){
+    return a - b;
+}
+function divide(a, b){
+    return a / b;
+}
+
+console.log(sum(1, 2))
