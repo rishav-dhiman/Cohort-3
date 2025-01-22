71963411d34795c182f0454af563f9a05753c94b
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-01-23 03:07:09 +0530

    Update 08_setTimeout.js

--- Web Dev/Week 02/08_setTimeout.js
+++ Web Dev/Week 02/08_setTimeout.js
@@ -4,6 +4,30 @@ function timeout(){
 
 console.log("Hi!");
 
-setTimeout(timeout, 15000);
+setTimeout(timeout, 1000);
 
 console.log("Welcome to Timeout")
+
+let c = 0;
+// 3-4 seconds
+for (let i = 0; i<1000000; i++) {
+    c = c + 1;
+}
+
+console.log("Expensive operation done");
+
+
+/* 
+    Option 1 - Correct
+    Hi!
+    Welcome to timeout
+    Expensive operation done
+    Click the button!
+
+    Option 2 - Not Correct
+    Hi!
+    Welcome to timeout
+    Click the button
+    Expensive operation done
+
+*/
