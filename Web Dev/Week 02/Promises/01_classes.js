7d406aeadbdade2dac8a4e5dde79bf1ff4e4277b
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-01-25 18:09:51 +0530

    Update 01_classes.js

--- Web Dev/Week 02/Promises/01_classes.js
+++ Web Dev/Week 02/Promises/01_classes.js
@@ -19,13 +19,11 @@ class Rectangle{
         console.log("Color is " + this.color);
     }
 
-    destroy(){
-
-    }
 }
 
 const rect = new Rectangle(2, 4, "Red"); 
 const rect2 = new Rectangle(20, 10, "Black"); 
 const area = rect.area();
 const area2 = rect2.area();
-console.log(area);
+const paint = rect.paint();
+console.log(area, area2, paint);
