742590cdc6d4eeaf5f8b7ae55f3c3ed2ca04a0cb
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-01-25 18:50:08 +0530

    Update 02_date_class.js

--- Web Dev/Week 02/Promises/02_date_class.js
+++ Web Dev/Week 02/Promises/02_date_class.js
@@ -1,3 +1,8 @@
 const date = new Date();
 console.log(date);
 console.log(date.getDate());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
