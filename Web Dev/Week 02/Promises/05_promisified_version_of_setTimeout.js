1042ee248f215448207f0f4b7a3360450b863e76
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-01-26 22:37:33 +0530

    Create 05_promisified_version_of_setTimeout.js

---
+++ Web Dev/Week 02/Promises/05_promisified_version_of_setTimeout.js
@@ -0,0 +1,25 @@
+// function waitFor3s(resolve) { // resolve = main
+//     setTimeout(resolve, 3000)
+// }
+
+// function setTimeoutPromisified() {
+//     return new Promise(waitFor3s); 
+// }
+
+// function main() {
+//     console.log("main is called");
+// }
+
+// setTimeoutPromisified().then(main); // promise based approach
+
+
+function random(resolve) { // resolve is also a function
+    setTimeout(resolve, 3000);
+}
+
+let p = new Promise(random); // supposed to return u something eventually
+
+function callback() {
+    console.log("promise succeeded");
+}
+p.then(callback);
