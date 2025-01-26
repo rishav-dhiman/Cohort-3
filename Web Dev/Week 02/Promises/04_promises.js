5e7ddd78666ae4c97b824cda141a2413d2af24d4
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-01-26 18:50:49 +0530

    Create 04_promises.js

---
+++ Web Dev/Week 02/Promises/04_promises.js
@@ -0,0 +1,27 @@
+// Promise class gives u a promise, that I will return u something in future
+
+//  settimeout is a promise, that I will return u something in future after the set time  
+//  setTimeout(fn, 3000)
+
+// callback based approach
+
+function logName() {
+    console.log("Harkirat");
+}
+
+setTimeout(logName, 3000);
+
+// promises based approach
+
+// defining a promise is hard, but calling a promise is easy
+// returns an object of the promise class
+function setTimeoutPromisified(ms) {
+    let p = new Promise(resolve => setTimeout(resolve, ms));
+    return p; // returns the object of the promise class which has been initiated
+}
+
+function callback() {
+    console.log("3 seconds have passed");
+}
+
+setTimeoutPromisified(3000).then(callback)
