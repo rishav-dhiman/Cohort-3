7995631ada25a3b3ffd72b27a8090d049a7bae03
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-02-03 18:13:15 +0530

    Create 04_assignment1.js

---
+++ Web Dev/Week 04/04_assignment1.js
@@ -0,0 +1,4 @@
+// Create a command line interface that lets the user specify a file path and the nodejs process counts the number of words inside it.
+
+// Input - node index.js /Users/kirat/file.txt
+// Output - You have 10 words in this file
