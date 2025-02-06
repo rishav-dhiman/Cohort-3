7649cd87e59f69f17cfdc3dd65e20a4b3930698b
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-02-06 16:00:22 +0530

    04-assignment

--- Web Dev/Week 04/04_assignment1.js
+++ Web Dev/Week 04/04_assignment1.js
@@ -1,4 +1,43 @@
-// Create a command line interface that lets the user specify a file path and the nodejs process counts the number of words inside it.
+// Create a command line/CLI interface that lets the user specify a file path and the nodejs process counts the number of words inside it.
 
 // Input - node index.js /Users/kirat/file.txt
 // Output - You have 10 words in this file
+
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('File based CLI')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
+
+program.command('count_words')
+  .description('Count the number of words in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const words = data.split(' ').length;
+        console.log(`There are ${words} words in ${file}`);
+      }
+    });
+  });
+  
+program.command('count_lines')
+  .description('Count the number of lines in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lines = data.split('\n').length;
+        console.log(`There are ${lines} lines in ${file}`);
+      }
+    });
+  });
+
+program.parse();
