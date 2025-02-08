d6702820582282682e39594b0c6666024cfeda57
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-02-08 17:25:41 +0530

    middleware

--- Web Dev/Week 04 HTTP/HTTP Offline/Middleware - Offline/index.js
+++ Web Dev/Week 04 HTTP/HTTP Offline/Middleware - Offline/index.js
@@ -2,38 +2,19 @@ const express = require('express');
 
 const app = express();
 
-// function that return a boolean if the age of the person is more than 14
-function isOldEnough(age) {
-    if (age >= 14) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
+// app.use(isOldEnoughMiddleware); // another way of defining middlewares below this
 
-app.get("/ride1", function(req, res) {
-    if (isOldEnough(req.query.age)) {
-        resizeBy.json({
-            msg: "You have successfully riden the ride 1"
-        })
-    } else {
-        res.status(411).json({
-            msg: "You are not old enough to ride this ride"
-        })
-    }
+app.get("/ride1", isOldEnoughMiddleware, function(req, res) {
+    res.json({
+        msg: "You have successfully riden the ride 1"
+    })
 })
 
-app.get("/ride2", function(req, res) {
-    if (isOldEnough(req.query.age)) {
-        resizeBy.json({
-            msg: "You have successfully riden the ride 2"
-        })
-    } else {
-        res.status(411).json({
-            msg: "You are not old enough to ride this ride"
-        })
-    }
+app.get("/ride2", isOldEnoughMiddleware, function(req, res) {
+    res.json({
+        msg: "You have successfully riden the ride 2"
+    })
 })
 
 app.listen(3000);
